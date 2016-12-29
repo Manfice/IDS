c9f4456aced2900363c237eb2296bac225043df7
Petr Povazhniy <ess_service@hotmail.com>
2016-12-29 16:06:56 +0300

    Console

---
+++ IndDev/localConsole/CrudProducts.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using IndDev.Domain.Context;
+using IndDev.Domain.Entity.Products;
+
+namespace localConsole
+{
+    public class CrudProducts
+    {
+        private DataContext _dbContext;
+
+        public CrudProducts()
+        {
+            _dbContext = new DataContext();
+        }
+
+        public string DeleteProduct(int id)
+        {
+            var p = _dbContext.Products.Find(id);
+
+            if (p==null)
+            {
+                return "Product was not found";
+            }
+            _dbContext.Prices.RemoveRange(p.Prices);
+            _dbContext.Products.Remove(p);
+            _dbContext.SaveChanges();
+            return $"Product with id {id} was successed deleted from database";
+        }
+
+        public List<Product> GetProductsWithNullArtOrTitle()
+        {
+            var result =
+                _dbContext.Products.Where(
+                    product => string.IsNullOrEmpty(product.Articul) || string.IsNullOrEmpty(product.Title)).ToList();
+            return result;
+        } 
+    }
+}
---
+++ IndDev/localConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using IndDev.Domain.Context;
+
+namespace localConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadLine()!="x")
+            {
+                Console.Write("Введите число: ");
+                var x = double.Parse(Console.ReadLine());
+                if (x%1 == 0)
+                {
+                    Console.WriteLine("Целое");
+                }
+                else
+                {
+                    Console.WriteLine("Не Целое");
+                }
+
+
+            }
+        }
+    }
+}
